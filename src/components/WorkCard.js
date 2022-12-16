8f14231ea251798f8a369978dc113b50eaaa826a
Md. Sowad Karim <sowad1999@gmail.com>
2022-12-17 01:03:34 +0600

    changed  my project section and favicon photo
    added my project link. Resource link and also added a favicon pic

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./WorkCardStyle.css"
 import pro1 from "../assets/project1.png"
 import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project5.png"
 import { NavLink } from 'react-router-dom'
 
 const WorkCard = (props) => {
@@ -38,13 +39,13 @@ const WorkCard = (props) => {
                     </div>
                 </div>
                 <div className="project-card" >
-                    <img src={pro2} alt='pro1' />
-                    <h2 className='project-title'>"ThIs iS buSSinEss lAndiNG PaGe"</h2>
+                    <img src={pro3} alt='pro1' />
+                    <h2 className='project-title'>"ThIs iS pOrtFoliO weB sItE foR Me"</h2>
                     <div className="pro-details">
-                        <p>It is a business static web page to give information to their client, amy inspires a grounded tangible and self affirming sense of wow i really can do this for over 1000 online entrepreneurs. Best selling marketing </p>
+                        <p>This is portfolio web site for me, where we showed others project to  enhance and share my knowledge with otherstheir client, amy inspires a grounded tangible and self affirming sense of wow i really can do this for over 1000 online entrepreneurs.</p>
                         <div className="pro-btns">
-                            <a href="https://captionstall.netlify.app/" className="btn">View</a>
-                            <a href="https://github.com/Md-SowadKarim/caption-stall" className="btn">Source</a>
+                            <a href="https://skfrontportfolio.netlify.app/" className="btn">View</a>
+                            <a href="https://github.com/Md-SowadKarim/Front-End-portfolio" className="btn">Source</a>
                         
                             
                         </div>
